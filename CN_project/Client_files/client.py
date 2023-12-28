c128ae5b8c81084e763889e09b7bd6613810654c
RezaCharrakh <116076795+RezaCharrakh@users.noreply.github.com>
2023-12-28 16:44:38 +0330

    Adding LIST command to client and server

--- CN_project/Client_files/client.py
+++ CN_project/Client_files/client.py
@@ -1,9 +1,12 @@
 import socket
+import os
+import datetime
+
 
 
 # Config
 IP = "127.0.0.1"
-PORT = 6156
+PORT = 2000
 BUFFER_SIZE = 1024
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,11 +19,42 @@ def connect():
     except:
         print("Couldn't connect! :(")
 
+
+def list_of_files():
+    print("Requesting files...\n")
+    try:
+        socket.sendall(b"LIST")
+    except:
+        print("Couldn't make server request! check Connection.")
+        return
+    try:
+        number_of_files = int.from_bytes(socket.recv(4), byteorder='big')
+        for i in range(number_of_files):
+            file_name_size = int.from_bytes(socket.recv(4), byteorder='big')
+            file_name = socket.recv(file_name_size).decode()
+            file_size = int.from_bytes(socket.recv(4), byteorder='big')
+            file_creation_time_str = socket.recv(20).decode()
+            file_creation_time = datetime.datetime.strptime(file_creation_time_str, '%Y-%m-%d %H:%M:%S')
+            print("\t* {} | {}b | {}".format(file_name, file_size, file_creation_time.strftime('%Y-%m-%d %H:%M:%S')))
+            socket.sendall(b"1")
+    except:
+        print("Couldn't retrieve listing")
+        return
+    try:
+        socket.sendall(b"1")
+        return
+    except:
+        print("Couldn't get final server confirmation")
+        return
+
+
 while True:
     # Listen for a command
     command = input("\nCommand -> ")
     if command.upper() == "CONN":
         connect()
+    elif command.upper() == "LIST":
+        list_of_files()
     elif command.upper() == "QUIT":
         break
     else:
--- CN_project/Server_files/server.py
+++ CN_project/Server_files/server.py
@@ -1,6 +1,10 @@
 import socket
+import os
 
 # Config
+import struct
+import sys
+
 IP = "127.0.0.1"
 PORT = 2000
 BUFFER_SIZE = 1024
@@ -12,11 +16,38 @@ conn, addr = socket.accept()
 
 print("\nConnected to by address: {}".format(addr))
 
+
+import os
+import datetime
+
+def list_files():
+    print("Listing files...")
+    listing = os.listdir(os.getcwd())
+    conn.send(len(listing).to_bytes(4, byteorder='big'))
+    for i in listing:
+        conn.send(len(i).to_bytes(4, byteorder='big'))
+        conn.send(i.encode())
+        conn.send(os.path.getsize(i).to_bytes(4, byteorder='big'))
+        file_creation_time = os.path.getctime(i)
+        file_creation_time_str = datetime.datetime.fromtimestamp(file_creation_time).strftime('%Y-%m-%d %H:%M:%S')
+        conn.send(file_creation_time_str.encode())
+        conn.recv(BUFFER_SIZE)
+    print("Successfully sent file listing")
+    return
+
+
+
 while True:
     data = conn.recv(BUFFER_SIZE).decode()
     print(f"\nReceived: {format(data)}")
+    if data == "LIST":
+        list_files()
     if data == "QUIT":
         break
     else:
         pass
     data = None
+
+
+
+
