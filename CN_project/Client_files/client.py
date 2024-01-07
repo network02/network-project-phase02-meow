189fb7ce754ad7390a172ec23c681b25a032ce22
RezaCharrakh <116076795+RezaCharrakh@users.noreply.github.com>
2024-01-07 18:53:56 +0330

    Adding data connection to program

--- CN_project/Client_files/client.py
+++ CN_project/Client_files/client.py
@@ -1,8 +1,9 @@
-import socket
+import socket as S
 import os
 import datetime
 import struct
 import sys
+import threading
 import time
 
 # Config
@@ -10,9 +11,9 @@ import struct
 import sys
 
 IP = "127.0.0.1"
-PORT = 2001
+PORT = 21
 BUFFER_SIZE = 1024
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = S.socket(S.AF_INET, S.SOCK_STREAM)
 
 
 def connect():
@@ -23,6 +24,41 @@ def connect():
         print("Connected successfully!")
     except:
         print("Couldn't connect! :(")
+
+
+def start_data_connection():
+    data_socket = S.socket(S.AF_INET, S.SOCK_STREAM)
+    data_socket.connect(("127.0.0.1", 20))
+    return data_socket
+
+
+def list_of_files():
+    print("Requesting files...\n")
+    try:
+        socket.sendall(b"LIST")
+        data_socket = start_data_connection()
+    except:
+        print("Couldn't make server request! check Connection.")
+        return
+    try:
+        number_of_files = int.from_bytes(data_socket.recv(4), byteorder='big')
+        for i in range(number_of_files):
+            file_name_size = int.from_bytes(data_socket.recv(4), byteorder='big')
+            file_name = data_socket.recv(file_name_size).decode()
+            file_size = int.from_bytes(data_socket.recv(4), byteorder='big')
+            file_creation_time_str = data_socket.recv(20).decode()
+            file_creation_time = datetime.datetime.strptime(file_creation_time_str, '%Y-%m-%d %H:%M:%S')
+            print("\t* {} | {}b | {}".format(file_name, file_size, file_creation_time.strftime('%Y-%m-%d %H:%M:%S')))
+            socket.sendall(b"1")
+    except:
+        print("Couldn't retrieve listing")
+        return
+    try:
+        socket.sendall(b"1")
+        return
+    except:
+        print("Couldn't get final server confirmation")
+        return
 
 
 def upload_file_on_server(fileName):
@@ -37,6 +73,7 @@ def upload_file_on_server(fileName):
     try:
         # Make upload request
         socket.send(b'STOR')
+        data_socket = start_data_connection()
     except:
         print("Couldn't make server request. Make sure a connection has been established.")
         return
@@ -46,13 +83,13 @@ def upload_file_on_server(fileName):
         socket.recv(BUFFER_SIZE)
         # Send file name size and file name
         fileNameSize = struct.pack(">h", sys.getsizeof(fileName))
-        socket.send(fileNameSize)
-        socket.send(fileName.encode('utf-8'))
+        data_socket.send(fileNameSize)
+        data_socket.send(fileName.encode('utf-8'))
         # Wait for server ok then send file size
         socket.recv(BUFFER_SIZE)
         filePath = os.path.realpath(fileName)
         fileSize = struct.pack(">i", os.path.getsize(filePath))
-        socket.send(fileSize)
+        data_socket.send(fileSize)
     except:
         print("Error sending file details")
     try:
@@ -61,12 +98,12 @@ def upload_file_on_server(fileName):
         l = content.read(BUFFER_SIZE)
         print("\nSending...")
         while l:
-            socket.send(l)
+            data_socket.send(l)
             l = content.read(BUFFER_SIZE)
         content.close()
         # Get upload performance details
-        uploadTime = struct.unpack(">f", socket.recv(4))[0]
-        uploadSize = struct.unpack(">i", socket.recv(4))[0]
+        uploadTime = struct.unpack(">f", data_socket.recv(4))[0]
+        uploadSize = struct.unpack(">i", data_socket.recv(4))[0]
         print("\nSent file: {}\nTime elapsed: {:.2f}s\nFile size: {:,}b".format(fileName, uploadTime, uploadSize))
     except:
         print("Error sending file")
@@ -80,6 +117,7 @@ def download_file_from_server(fileName):
     try:
         # Send download request to the server
         socket.send(b'RETR')
+        data_socket = start_data_connection()
     except:
         print("Couldn't make server request. Make sure a connection has been established.")
         return
@@ -89,11 +127,11 @@ def download_file_from_server(fileName):
 
         # Send file name length and file name to the server
         fileNameSize = struct.pack(">h", sys.getsizeof(fileName))
-        socket.send(fileNameSize)
-        socket.send(fileName.encode('utf-8'))
+        data_socket.send(fileNameSize)
+        data_socket.send(fileName.encode('utf-8'))
 
         # Receive file size from the server
-        fileSize = struct.unpack(">i", socket.recv(4))[0]
+        fileSize = struct.unpack(">i", data_socket.recv(4))[0]
         if fileSize == -1:
             # If file size is -1, file doesn't exist
             print("File does not exist. Make sure the name was entered correctly")
@@ -114,59 +152,35 @@ def download_file_from_server(fileName):
         bytesReceived = 0
         print("\nDownloading...")
         while bytesReceived < fileSize:
-            l = socket.recv(BUFFER_SIZE)
+            l = data_socket.recv(BUFFER_SIZE)
             outputFile.write(l)
             bytesReceived += BUFFER_SIZE
+            print("while")
 
         # Close the file handler
+        socket.sendall(b"1")
+
         outputFile.close()
 
         # Send acknowledgement to receive download details
         socket.send(b'1')
 
         # Receive download performance details
-        downloadTime = struct.unpack(">f", socket.recv(4))[0]
-        downloadSize = struct.unpack(">i", socket.recv(4))[0]
-        print("Successfully downloaded {}.\nTime elapsed: {:.2f}s\nFile size: {:,}b".format(fileName, downloadTime,
-                                                                                            downloadSize))
+        # downloadTime = struct.unpack(">f", socket.recv(4))[0]
+        # downloadSize = struct.unpack(">i", socket.recv(4))[0]
+        print(f"{fileName} Successfully downloaded")
     except:
         print("Error downloading file")
         return
     return
 
 
-def list_of_files():
-    print("Requesting files...\n")
-    try:
-        socket.sendall(b"LIST")
-    except:
-        print("Couldn't make server request! check Connection.")
-        return
-    try:
-        number_of_files = int.from_bytes(socket.recv(4), byteorder='big')
-        for i in range(number_of_files):
-            file_name_size = int.from_bytes(socket.recv(4), byteorder='big')
-            file_name = socket.recv(file_name_size).decode()
-            file_size = int.from_bytes(socket.recv(4), byteorder='big')
-            file_creation_time_str = socket.recv(20).decode()
-            file_creation_time = datetime.datetime.strptime(file_creation_time_str, '%Y-%m-%d %H:%M:%S')
-            print("\t* {} | {}b | {}".format(file_name, file_size, file_creation_time.strftime('%Y-%m-%d %H:%M:%S')))
-            socket.sendall(b"1")
-    except:
-        print("Couldn't retrieve listing")
-        return
-    try:
-        socket.sendall(b"1")
-        return
-    except:
-        print("Couldn't get final server confirmation")
-        return
-
 
 def delete_file(file_name):
     print("Deleting file: {}...".format(file_name))
     try:
         socket.sendall(b"DELE")
+        data_socket = start_data_connection()
         socket.recv(BUFFER_SIZE)
     except:
         print("Couldn't connect to the server. Make sure a connection has been established.")
@@ -176,10 +190,10 @@ def delete_file(file_name):
         format_string = "h"
         value = sys.getsizeof(file_name)
         packed_data = struct.pack(format_string, value)
-        socket.sendall(packed_data)
+        data_socket.sendall(packed_data)
 
         # Send the file name
-        socket.sendall(file_name.encode())
+        data_socket.sendall(file_name.encode())
     except:
         print("Couldn't send file details")
         return None
@@ -224,15 +238,16 @@ def create_directory(directory_name):
     try:
         # Send request, then wait for go-ahead
         socket.sendall(b"MKD")
+        data_socket = start_data_connection()
         socket.recv(BUFFER_SIZE)
     except:
         print("Couldn't connect to the server. Make sure a connection has been established.")
         return None
     try:
         # Send directory path name length, then directory path name
-        socket.sendall(struct.pack("h", sys.getsizeof(directory_name)))
+        data_socket.sendall(struct.pack("h", sys.getsizeof(directory_name)))
         socket.recv(BUFFER_SIZE)
-        socket.sendall(directory_name.encode())
+        data_socket.sendall(directory_name.encode())
         socket.recv(BUFFER_SIZE)
     except:
         print("Couldn't send Directory details")
@@ -246,23 +261,128 @@ def create_directory(directory_name):
     return None
 
 
+def remove_directory(directory_name):
+    print("Removing Directory: {}...".format(directory_name))
+    try:
+        socket.sendall(b"RMD")
+        data_socket = start_data_connection()
+        socket.recv(BUFFER_SIZE)
+    except:
+        print("Couldn't connect to the server. Make sure a connection has been established.")
+        return None
+    try:
+        data_socket.sendall(struct.pack("h", sys.getsizeof(directory_name)))
+        data_socket.sendall(directory_name.encode())
+        socket.recv(BUFFER_SIZE)
+    except:
+        print("Couldn't send Directory details")
+        return None
+
+    directory_check = int(socket.recv(BUFFER_SIZE).decode())
+    if directory_check:
+        print("directory removed successfully.")
+    else:
+        print("couldn't remove the directory.")
+    return None
+
+
+def change_directory(new_path):
+    print("Changing Directory: {}...".format(new_path))
+    try:
+        # Send request, then wait for go-ahead
+        socket.sendall(b"CWD")
+        data_socket = start_data_connection()
+    except:
+        print("Couldn't connect to the server. Make sure a connection has been established.")
+        return None
+    try:
+        # Send directory path name length, then directory path name
+        data_socket.sendall(struct.pack("h", sys.getsizeof(new_path)))
+        data_socket.sendall(new_path.encode())
+    except:
+        print("Couldn't send Path details")
+        return None
+
+    change_check = int(socket.recv(BUFFER_SIZE).decode())
+    if change_check:
+        print("path changed successfully.")
+    else:
+        print("couldn't change path.")
+    return None
+
+
+def display_current_directory():
+    try:
+        # Send request, then wait for go-ahead
+        # data_socket_client = send_pasv_command(socket)
+        socket.sendall(b"PWD")
+        data_socket = start_data_connection()
+    except:
+        print("Couldn't connect to the server. Make sure a connection has been established.")
+        return None
+    try:
+        # Send directory path name length, then directory path name
+        path_len = int(data_socket.recv(BUFFER_SIZE).decode())
+        path = data_socket.recv(path_len).decode()
+        print(path)
+    except:
+        print("Couldn't receive path.")
+        return None
+    return None
+
+
+def change_directory_up():
+    try:
+        socket.sendall(b"CDUP")
+    except:
+        print("Couldn't connect to the server. Make sure a connection has been established.")
+        return None
+
+    change_check = int(socket.recv(BUFFER_SIZE).decode())
+    if change_check:
+        print("path changed successfully.")
+        display_current_directory()
+    else:
+        print("couldn't change path.")
+    return None
+
+
 while True:
     # Listen for a command
-    command = input("\nCommand -> ")
+    command = input("\nCommand -> ").split()
 
-    if command.upper() == "CONN":
+    if command[0].upper() == "CONN":
         connect()
 
-    elif command.upper() == "LIST":
+    elif command[0].upper() == "LIST":
         list_of_files()
 
-    elif command.upper() == "STOR":
-        fileName = input("file name: ")
-        upload_file_on_server(fileName)
-    elif command.upper() == "RETR":
-        fileName = input("file name: ")
-        download_file_from_server(fileName)
-    elif command.upper() == "QUIT":
+    elif command[0].upper() == "STOR":
+        upload_file_on_server(input("File name: "))
+
+    elif command[0].upper() == "RETR":
+        download_file_from_server(input("File name: "))
+
+    elif command[0].upper() == "MKD":
+        create_directory(input("Folder name: "))
+
+    elif command[0].upper() == "DELE":
+        delete_file(input("File name: "))
+
+    elif command[0].upper() == "RMD":
+        remove_directory(command[1])
+
+    elif command[0].upper() == "PWD":
+        display_current_directory()
+
+    elif command[0].upper() == "CWD":
+        change_directory(command[1])
+
+    elif command[0].upper() == "CDUP":
+        change_directory_up()
+
+    elif command[0].upper() == "QUIT":
         break
+
     else:
         print("Enter a valid command!")
--- CN_project/Server_files/server.py
+++ CN_project/Server_files/server.py
@@ -1,48 +1,60 @@
-import socket
+import socket as S
 import os
 import datetime
-
-# Config
 import struct
 import sys
 import time
 
+
 IP = "127.0.0.1"
-PORT = 2001
+PORT = 21
 BUFFER_SIZE = 1024
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = S.socket(S.AF_INET, S.SOCK_STREAM)
 socket.bind((IP, PORT))
 socket.listen(5)
+
 print(f"\nFTP server is online on {IP}:{PORT}!\n")
 conn, addr = socket.accept()
+# print("\nConnected to by address: {}".format(addr))
 
-print("\nConnected to by address: {}".format(addr))
+
+
+def start_data_connection():
+    # portRandom = random.randrange(3000, 50000)
+    data_socket = S.socket(S.AF_INET, S.SOCK_STREAM)
+    data_socket.bind(("127.0.0.1", 20))
+    data_socket.listen()
+    conn2, addr = data_socket.accept()
+    return conn2
 
 def list_files():
+    conn2 = start_data_connection()
     print("Listing files...")
     listing = os.listdir(os.getcwd())
-    conn.send(len(listing).to_bytes(4, byteorder='big'))
+    conn2.send(len(listing).to_bytes(4, byteorder='big'))
     for i in listing:
-        conn.send(len(i).to_bytes(4, byteorder='big'))
-        conn.send(i.encode())
-        conn.send(os.path.getsize(i).to_bytes(4, byteorder='big'))
+        conn2.send(len(i).to_bytes(4, byteorder='big'))
+        conn2.send(i.encode())
+        conn2.send(os.path.getsize(i).to_bytes(4, byteorder='big'))
         file_creation_time = os.path.getctime(i)
         file_creation_time_str = datetime.datetime.fromtimestamp(file_creation_time).strftime('%Y-%m-%d %H:%M:%S')
-        conn.send(file_creation_time_str.encode())
+        conn2.send(file_creation_time_str.encode())
         conn.recv(BUFFER_SIZE)
     print("Successfully sent file listing")
     return
 
+
 def store_file_in_server():
+    conn2 = start_data_connection()
     # Send message once server is ready to recieve file details
     conn.send(b'1')
     # Recieve file name length, then file name
-    fileNameLength = struct.unpack(">h", conn.recv(2))[0]
-    fileName = conn.recv(fileNameLength).decode('utf-8')
+    fileNameLength = struct.unpack(">h", conn2.recv(2))[0]
+    fileName = conn2.recv(fileNameLength).decode('utf-8')
     # Send message to let client know server is ready for document content
     conn.send(b'1')
     # Recieve file size
-    fileSize = struct.unpack(">i", conn.recv(4))[0]
+    fileSize = struct.unpack(">i", conn2.recv(4))[0]
     # Initialise and enter loop to recieve file content
     start_time = time.time()
     outputFile = open(fileName, 'wb')
@@ -50,31 +62,33 @@ def store_file_in_server():
     bytesReceived = 0
     print("\nReceiving...")
     while bytesReceived < fileSize:
-        l = conn.recv(BUFFER_SIZE)
+        l = conn2.recv(BUFFER_SIZE)
         outputFile.write(l)
         bytesReceived += BUFFER_SIZE
     outputFile.close()
     print("\nReceived file: {}".format(fileName))
     # Send upload performance details
-    conn.send(struct.pack('>f', time.time() - start_time))
-    conn.send(struct.pack('>i', fileSize))
+    conn2.send(struct.pack('>f', time.time() - start_time))
+    conn2.send(struct.pack('>i', fileSize))
     return
 
+
 def download_file_from_server():
+    conn2 = start_data_connection()
     # Send message indicating readiness to receive file details
     conn.send(b'1')
 
     # Receive file name length and extract it
-    fileNameLength = struct.unpack(">h", conn.recv(2))[0]
+    fileNameLength = struct.unpack(">h", conn2.recv(2))[0]
 
     # Receive the entire file name from the client
-    fileName = conn.recv(fileNameLength).decode('utf-8')
+    fileName = conn2.recv(fileNameLength).decode('utf-8')
 
     # Check if the file exists on the server
     if os.path.isfile(fileName):
         # If the file exists, send its size to the client
         fileSize = struct.pack(">i", os.path.getsize(fileName))
-        conn.send(fileSize)
+        conn2.send(fileSize)
     else:
         # If the file doesn't exist, send an error code to the client
         print("File name not valid")
@@ -94,23 +108,31 @@ def download_file_from_server():
     # Read the file in chunks of BUFFER_SIZE and send them to the client
     l = content.read(BUFFER_SIZE)
     while l:
-        conn.send(l)
+        print("server while")
+        conn2.send(l)
         l = content.read(BUFFER_SIZE)
 
     # Close the file handle
+    conn.recv(BUFFER_SIZE)
+
     content.close()
 
     # Receive the client's go-ahead before sending download details
     conn.recv(BUFFER_SIZE)
 
+
     # Send the download time to the client
-    conn.sendall(struct.pack(">f", time.time() - start_time))
+    # conn.sendall(struct.pack(">f", time.time() - start_time))
+    print(f"{fileName} Successfully downloaded")
+
     return
 
+
 def delete_file():
+    conn2 = start_data_connection()
     conn.sendall(b"1")
-    file_name_length = struct.unpack("h", conn.recv(2))[0]
-    file_name = conn.recv(file_name_length).decode()
+    file_name_length = struct.unpack("h", conn2.recv(2))[0]
+    file_name = conn2.recv(file_name_length).decode()
     if os.path.isfile(file_name):
         conn.sendall(struct.pack("i", 1))
     else:
@@ -130,13 +152,14 @@ def delete_file():
 
 
 def create_directory():
+    conn2 = start_data_connection()
     # Send go-ahead
     conn.sendall(b"1")
     # Get Directory details
-    directory_name_length = struct.unpack("h", conn.recv(2))[0]
+    directory_name_length = struct.unpack("h", conn2.recv(2))[0]
     print(directory_name_length)
     conn.sendall(b"1")
-    directory_name = conn.recv(directory_name_length).decode()
+    directory_name = conn2.recv(directory_name_length).decode()
     print(directory_name)
     conn.sendall(b"1")
     try:
@@ -148,16 +171,93 @@ def create_directory():
     return None
 
 
+def remove_directory():
+    conn2 = start_data_connection()
+    # Send go-ahead
+    conn.sendall(b"1")
+    # Get Directory details
+    directory_name_length = struct.unpack("h", conn2.recv(2))[0]
+    directory_name = conn2.recv(directory_name_length).decode()
+    print(directory_name)
+    conn.sendall(b"1")
+    try:
+        os.rmdir(directory_name)
+        conn.sendall(b"1")
+    except OSError as error:
+        print(error)
+        conn.sendall(b"0")
+    return None
+
+
+def change_directory():
+    # Send go-ahead
+    # Get Directory details
+    conn2 = start_data_connection()
+    new_path_length = struct.unpack("h", conn2.recv(2))[0]
+    new_path = conn2.recv(new_path_length).decode()
+    try:
+        os.chdir(new_path)
+        conn.sendall(b"1")
+    except OSError as error:
+        print(error)
+        conn.sendall(b"0")
+    return None
+
+
+def display_current_directory():
+    try:
+        conn2 = start_data_connection()
+        # Send response to client
+        conn.sendall(b'150 Opening data connection.')
+        cwd = os.getcwd()
+        print(cwd)
+        conn2.sendall(str(sys.getsizeof(cwd)).encode())
+        conn2.sendall(cwd.encode())
+
+        conn2.close()
+        conn.sendall('226 Transfer complete.'.encode('utf-8'))
+    except OSError as error:
+        print(error)
+    except Exception as e:
+        print(e)
+        print("couldn't send path.")
+    return None
+
+
+def change_directory_up():
+    try:
+        os.chdir('../')
+        conn.sendall(b"1")
+    except OSError as error:
+        print(error)
+        conn.sendall(b"0")
+    return None
+
 
 while True:
+
     data = conn.recv(BUFFER_SIZE).decode()
     print(f"\nReceived: {format(data)}")
     if data == "LIST":
         list_files()
     if data == "STOR":
-            store_file_in_server()
+        store_file_in_server()
     if data == "RETR":
-            download_file_from_server()
+        download_file_from_server()
+    if data == "DELE":
+        delete_file()
+    if data == "MKD":
+        create_directory()
+    if data == "RMD":
+        remove_directory()
+    if data == "PWD":
+        display_current_directory()
+    if data == "CWD":
+        change_directory()
+    if data == "CDUP":
+        change_directory_up()
+    if data == "PASV":
+        data_socket = start_data_connection()
     if data == "QUIT":
         pass
     data = None
